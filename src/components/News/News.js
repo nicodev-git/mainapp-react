3f220a0974babd7f35c3ceaa4bd2912bbf937810
nicodev-git <schaefernicker@gmail.com>
2021-04-28 14:37:02 +0500

    Added news content

---
+++ src/components/News/News.js
@@ -0,0 +1,18 @@
+import React, {useContext} from 'react';
+import {NewsContext} from './NewsContext';
+import NewsArticle from './NewsArticle';
+
+function News(props) {
+    const {data} = useContext(NewsContext);
+    console.log(data);
+
+    return (
+        <div>
+            <div className="all-news">
+                {data ? data.articles.map((news) => (<NewsArticle data={news} key={news.url}/>)) : "Loading..."}
+            </div>
+        </div>
+    );
+};
+
+export default News;
---
+++ src/components/News/NewsContext.js
@@ -0,0 +1,23 @@
+import React, {createContext, useEffect, useState} from 'react';
+import axios from 'axios';
+
+export const NewsContext = createContext();
+
+export const NewsContextProvider = (props) => {
+    const [data, setData] = useState();
+    const apiKey = "55ebc220291141b98982efbcffb5aa52";
+
+    useEffect(() => {
+        axios.get
+        (
+            `http://newsapi.org/v2/everything?q=top-headlines&sortBy=publishedAt&apiKey=${apiKey}`
+        ).then(response => setData(response.data))
+        .catch((error) => console.log(error));
+    }, []);
+
+    return(
+        <NewsContext.Provider value={{data}}>
+            {props.children}
+        </NewsContext.Provider>
+    );
+};
