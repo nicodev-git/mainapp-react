50aa5c7e98b3427b369bfbadd5e5b22d5ab7d43a
nicodev-git <schaefernicker@gmail.com>
2021-05-06 00:46:34 +0500

    Testing API

--- src/components/News/NewsContext.js
+++ src/components/News/NewsContext.js
@@ -6,14 +6,16 @@ export const NewsContext = createContext();
 export const NewsContextProvider = (props) => {
     const [data, setData] = useState();
     const apiKey = "55ebc220291141b98982efbcffb5aa52";
-
+    var query = "top-headlines";
+    var param=JSON.stringify({"category":"local"});
     useEffect(() => {
         axios.get
         (
-            `http://newsapi.org/v2/everything?q=top-headlines&sortBy=publishedAt&apiKey=${apiKey}`
-        ).then(response => setData(response.data))
+            "http://127.0.0.1:5000/news"
+        ).then(response => setData(response.data["news"]))
         .catch((error) => console.log(error));
-    }, []);
+    });
+    console.log(query)
 
     return(
         <div className="">
