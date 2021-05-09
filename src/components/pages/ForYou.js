bf0f908a141d426ab7f0a5b1d195fb9eb4cdc97b
nicodev-git <schaefernicker@gmail.com>
2021-05-10 01:04:53 +0500

    Temporary News Removal

--- src/components/pages/ForYou.js
+++ src/components/pages/ForYou.js
@@ -1,13 +1,10 @@
 import React from 'react';
-import News from '../News/News';
-import {NewsContextProvider} from '../News/NewsContext'
+
 
 function ForYou() {
     return (
         <div className="foryou">
-            <NewsContextProvider query="local">
-                <News/>
-            </NewsContextProvider>
+        
         </div>
     )
 }
