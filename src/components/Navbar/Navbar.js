86b14de6e1328bc3c646267874a90d3f2e8cc168
nicodev-git <schaefernicker@gmail.com>
2021-05-18 22:34:02 +0500

    Added Navs

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -12,96 +12,73 @@ class Navbar extends Component {
 
     render() {
         return(
-            <nav className="navbar navbar-expand-lg navbar-light bg-light justify-content-between">
+            <nav>
+                <div className="navbar navbar-expand-lg navbar-light bg-light justify-content-between">
+                    <div>
+                        <a className="navbar-logo" href="/home">
+                            <h1>Newsly</h1>
+                        </a>
+                        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                            <span class="navbar-toggler-icon"></span>
+                        </button>
+                    </div>
+                    <div action="/action_page.php">
+                        <form className="form-inline my-2 my-lg-2">
+                            <input
+                                className="form-control mr-sm-2" 
+                                type="search" 
+                                placeholder="Search..."
+                                aria-label="Search"
+                            />
+                            <button
+                                className="btn-light btn-sm"
+                                type="submit"><i class="fa fa-search"></i></button>
+                        </form>
+                    </div>
+                    <div>
+                        <a href="/login">
+                            <button type="button" className="btn btn-primary">Log In</button>
+                        </a>
+                    </div>
+                    <div>
+                        <a href="/signup">
+                            <button type="button" className="btn btn-light">Sign Up</button>
+                        </a>
+                    </div>
+                    </div>
                 <div>
-                    <a className="navbar-logo" href="/home">
-                        <h1>Newsly</h1>
-                    </a>
-                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                        <span class="navbar-toggler-icon"></span>
-                    </button>
-                </div>
-                <div action="/action_page.php">
-                    <form className="form-inline my-2 my-lg-2">
-                        <input
-                            className="form-control mr-sm-2" 
-                            type="search" 
-                            placeholder="Search..."
-                            aria-label="Search"
-                        />
-                        <button
-                            className="btn--outline-success"
-                            type="submit"><i class="fa fa-search"></i></button>
-                    </form>
-                </div>
-                <div>
-                    <a href="/login">
-                        <button type="button" className="btn btn-primary">Log In</button>
-                    </a>
-                </div>
-                <div>
-                    <a href="/signup">
-                        <button type="button" className="btn btn-light">Sign Up</button>
-                    </a>
-                </div>
-                <div className="container">
-                    <ul>
-                        <div className="dropdown">
-                            <a href="">
-                                <button className="dropbtn">
-                                    News
-                                    <i className="fa fa-caret-down"></i>
-                                    <span>&#9660;</span>
-                                </button>
-                            </a>
-                            <div className="dropdown-content">
-                                <a href="/business">Business</a>
-                                <a href="/technology">Technology</a>
-                                <a href="/sports">Sports</a>
-                                <a href="/science">Science</a>
-                                <a href="/health">Health</a>
+                    <ul className="nav nav-pills justify-content-center">
+                        <li className="nav-item dropdown">
+                            <a className="nav-link dropdown-toggle" data-toggle="dropdown" href="" role="button" aria-haspopup="true" aria-expanded="false">News</a>
+                            <div className="dropdown-menu">
+                                <a className="dropdown-item" href="/business">Business</a>
+                                <a className="dropdown-item" href="/technology">Technology</a>
+                                <a className="dropdown-item" href="/sports">Sports</a>
+                                <a className="dropdown-item" href="/science">Science</a>
+                                <a className="dropdown-item" href="/health">Health</a>
                             </div>
-                        </div>
-                        <div className="dropdown">
-                            <a href="">
-                                <button className="dropbtn">
-                                    User
-                                    <i className="fa fa-caret-down"></i>
-                                    <span>&#9660;</span>
-                                </button>
-                            </a>
-                            <div className="dropdown-content">
-                                <a href="/foryou">For You</a>
-                                <a href="/localnews">Local News</a>
+                        </li>
+                        <li className="nav-item dropdown">
+                            <a className="nav-link dropdown-toggle" data-toggle="dropdown" href="" role="button" aria-haspopup="true" aria-expanded="false">User</a>
+                            <div className="dropdown-menu">
+                                <a className="dropdown-item" href="/foryou">For You</a>
+                                <a className="dropdown-item" href="/localnews">Local News</a>
                             </div>
-                        </div>
-                        <div className="dropdown">
-                            <a href="/topheadlines">
-                                <button className="dropbtn">
-                                    Top Headlines
-                                    <span>&#9660;</span>
-                                </button>
-                            </a>
-                        </div>
-                        <div className="dropdown">
-                            <a href="/pakistan">
-                                <button className="dropbtn">
-                                    Pakistan
-                                    <span>&#9660;</span>
-                                </button>
-                            </a>
-                        </div>
-                        <div className="dropdown">
-                            <a href="/world">
-                                <button className="dropbtn">
-                                    World
-                                    <span>&#9660;</span>
-                                </button>
-                            </a>
-                        </div>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="/topheadlines">Top Headlines</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="/pakistan">Pakistan</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="/world">World</a>
+                        </li>
                     </ul>
                 </div>
+                
             </nav>
+            
         );
     };
 };
