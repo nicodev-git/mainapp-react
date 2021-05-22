7b0cbd732215f183fe89a16bada81cd3a6aa8ded
MastProTech <uzairarif1024@gmail.com>
2021-05-22 16:23:35 +0000

    Improved design

--- src/components/Auth/Signup.js
+++ src/components/Auth/Signup.js
@@ -5,75 +5,137 @@ import './Signup.css';
 
 const Signup = () => {
     return (
-        <div className="signup">
-            <div className="container">
-                <div className="signup-box">
-                    <form onSubmit = {(e) => signup(e)}>
-                        <div className="small-box">
-                            <div className="login-head">
-                                <h1 className="signup-heading">Sign Up!</h1>
-                            </div>
-                            <div className="box">
-                                <input
-                                    className="enter-name"
-                                    type="text"
-                                    id="inputUsername"
-                                    placeholder="Username"
-                                    name="name"
-                                    required
-                                />
-                                <input
-                                    className="enter-email"
-                                    type="text"
-                                    id="inputEmail"
-                                    name="email"
-                                    placeholder="Email"
-                                    required
-                                />
-                                <input
-                                    className="enter-pass"
-                                    type="text"
-                                    id="inputPassword"
-                                    placeholder="Password"
-                                    name="psd"
-                                    required
-                                />
-                                <input
-                                    class="remember-pass"
-                                    type="password"
-                                    id="confirmInputPassword"
-                                    placeholder="Confirm Password"
-                                    name="repeat-pass"
-                                    required
-                                />
-                            </div>
-                            <div className="remember">
-                                {/* <label>
-                                    <input
-                                        className="remember"
-                                        type="checkbox"
-                                        checked="checked"
-                                        name="remember"
-                                    />
-                                    Remember Me
-                                </label> */}
-                            </div>
-                            <div className="privacy-term">
-                                <p>
-                                    By creating an account, you therefore agree with our 
-                                    <a href="#">Terms and Conditions</a>.
-                                </p>
-                            </div>
-                            <div className="clearfix">
-                                {/* <button type="button" className="cancelButton">Cancel</button>
-                                <button type="submit" className="signUpButton">Sign Up</button> */}
-                                <input className="signUpButton" type="submit"/>
-                            </div>
-                        </div>
-                    </form>
-                </div>
+        <div>
+            <div className="signup-box">
+                <form onSubmit = {(e) => signup(e)}>
+                    <div className="signup-heading">
+                        <div className="signup text-center">Sign up to Newsly</div>
+                    </div>
+                    <input
+                        className="enter-name"
+                        type="text"
+                        id="inputUsername"
+                        placeholder="Username"
+                        name="name"
+                        required
+                    />
+                    <input
+                        className="enter-email"
+                        type="text"
+                        id="inputEmail"
+                        name="email"
+                        placeholder="Email"
+                        required
+                    />
+                    <input
+                        className="enter-pass"
+                        type="text"
+                        id="inputPassword"
+                        placeholder="Password"
+                        name="psd"
+                        required
+                    />
+                    <input
+                        className="confirm-pass"
+                        type="password"
+                        id="confirmInputPassword"
+                        placeholder="Confirm Password"
+                        name="repeat-pass"
+                        required
+                    />
+                    <div className="remember">
+                        <label>
+                            <input
+                                className="remember mr-1"
+                                type="checkbox"
+                                name="remember"
+                            />
+                            Remember Me
+                        </label>
+                    </div>
+                    <div className="privacy-term">
+                        <p>
+                            By creating an account, you therefore agree with our 
+                            <a href="#"> Terms and Conditions</a>.
+                        </p>
+                    </div>
+                    <div className="div-btn-login">
+                        <a href="/">
+                            <input className="btn btn-signup bg-primary text-light" type="submit" value="Sign Up"/>
+                        </a>
+                    </div>
+                </form>
             </div>
-        </div>
+        </div>            
+        // <div className="signup">
+        //     <div className="container">
+        //         <div className="signup-box">
+        //             <form onSubmit = {(e) => signup(e)}>
+        //                 <div className="small-box">
+        //                     <div className="login-head">
+        //                         <h1 className="signup-heading">Sign Up!</h1>
+        //                     </div>
+        //                     <div className="box">
+                            //     <input
+                            //         className="enter-name"
+                            //         type="text"
+                            //         id="inputUsername"
+                            //         placeholder="Username"
+                            //         name="name"
+                            //         required
+                            //     />
+                            //     <input
+                            //         className="enter-email"
+                            //         type="text"
+                            //         id="inputEmail"
+                            //         name="email"
+                            //         placeholder="Email"
+                            //         required
+                            //     />
+                            //     <input
+                            //         className="enter-pass"
+                            //         type="text"
+                            //         id="inputPassword"
+                            //         placeholder="Password"
+                            //         name="psd"
+                            //         required
+                            //     />
+                            //     <input
+                            //         class="remember-pass"
+                            //         type="password"
+                            //         id="confirmInputPassword"
+                            //         placeholder="Confirm Password"
+                            //         name="repeat-pass"
+                            //         required
+                            //     />
+                            // </div>
+                            // <div className="remember">
+                            //     {/* <label>
+                            //         <input
+                            //             className="remember"
+                            //             type="checkbox"
+                            //             checked="checked"
+                            //             name="remember"
+                            //         />
+                            //         Remember Me
+                            //     </label> */}
+                            // </div>
+                            // <div className="privacy-term">
+                            //     <p>
+                            //         By creating an account, you therefore agree with our 
+                            //         <a href="#">Terms and Conditions</a>.
+                            //     </p>
+                            // </div>
+        //                     <div className="clearfix">
+        //                         {/* <button type="button" className="cancelButton">Cancel</button>
+        //                         <button type="submit" className="signUpButton">Sign Up</button> */}
+        //                         <input className="signUpButton" type="submit"/>
+        //                     </div>
+        //                 </div>
+        //             </form>
+        //         </div>
+        //     </div>
+        // </div>
     );
 };
 
