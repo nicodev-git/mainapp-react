ead3aff9fe562c967d27144cd5cba312c295f747
nicodev-git <schaefernicker@gmail.com>
2021-05-22 13:36:22 +0500

    Added page for Urdu

---
+++ src/components/pages/Urdu.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Urdu  = () => {
+    return (
+        <div>
+            <h1>Urdu News are being displayed here...</h1>
+        </div>
+    )
+}
+
+export default Urdu;
