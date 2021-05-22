95a3debaeb404e7be12bcd80e25badf971bab298
MastProTech <uzairarif1024@gmail.com>
2021-05-22 13:02:18 +0000

    Improved Navbar design

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 // import { MenuItems } from "./MenuItems";
 // import { Button } from "../Button";
-// import './Navbar.css';
+import './Navbar.css';
 
 class Navbar extends Component {
     // state = { clicked: false }
@@ -13,39 +13,37 @@ class Navbar extends Component {
     render() {
         return(
             <nav>
-                <div className="navbar navbar-expand-lg navbar-light bg-light justify-content-between">
-                    <div>
-                        <a className="navbar-logo" href="/home">
+                <div className="navbar navbar-expand-lg navbar-light bg-primary row">
+                    <div className="col-4">
+                        <a className="navbar-logo text-light" href="/home">
                             <h1>Newsly</h1>
                         </a>
-                        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        {/* <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                             <span class="navbar-toggler-icon"></span>
-                        </button>
+                        </button> */}
                     </div>
-                    <div action="/action_page.php">
-                        <form className="form-inline my-2 my-lg-2">
+                    <div className="col-4" action="/action_page.php">
+                        <form className="input-group my-2 my-lg-2">
                             <input
-                                className="form-control mr-sm-2" 
+                                className="form-control inp-search bg-primary text-light" 
                                 type="search" 
                                 placeholder="Search..."
                                 aria-label="Search"
                             />
                             <button
-                                className="btn-light btn-sm"
+                                className="btn-search bg-primary text-light p-1"
                                 type="submit"><i class="fa fa-search"></i></button>
                         </form>
                     </div>
-                    <div>
-                        <a href="/login">
-                            <button type="button" className="btn btn-primary">Log In</button>
+                    <div className="col-4">
+                        <a className="float-right" href="/login">
+                            <button type="button" className="btn btn-login text-light p-2">Log In</button>
+                        </a>
+                        <a className="float-right" href="/signup">
+                            <button type="button" className="btn text-light p-2">Sign Up</button>
                         </a>
                     </div>
-                    <div>
-                        <a href="/signup">
-                            <button type="button" className="btn btn-light">Sign Up</button>
-                        </a>
-                    </div>
-                    </div>
+                </div>
                 <div>
                     <ul className="nav nav-pills justify-content-center">
                         <li className="nav-item dropdown">
