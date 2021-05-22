8e7627de85e6de5db72354cb8d4ae8a8c1743ff5
MastProTech <uzairarif1024@gmail.com>
2021-05-22 16:23:49 +0000

    Minor bug fixes

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -13,16 +13,16 @@ class Navbar extends Component {
     render() {
         return(
             <nav>
-                <div className="navbar navbar-expand-lg navbar-light bg-primary row">
+                <div className="navbar navbar-expand-lg navbar-light bg-primary">
                     <div className="col-4">
-                        <a className="navbar-logo text-light" href="/home">
+                        <a className="navbar-logo text-light" href="/">
                             <h1>Newsly</h1>
                         </a>
                         {/* <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                             <span class="navbar-toggler-icon"></span>
                         </button> */}
                     </div>
-                    <div className="col-4" action="/action_page.php">
+                    <div className="col-3 col-md-4" action="/action_page.php">
                         <form className="input-group my-2 my-lg-2">
                             <input
                                 className="form-control inp-search bg-primary text-light" 
@@ -31,16 +31,16 @@ class Navbar extends Component {
                                 aria-label="Search"
                             />
                             <button
-                                className="btn-search bg-primary text-light p-1"
+                                className="btn btn-search bg-primary text-light p-1"
                                 type="submit"><i class="fa fa-search"></i></button>
                         </form>
                     </div>
-                    <div className="col-4">
+                    <div className="col-5 col-md-4">
                         <a className="float-right" href="/login">
-                            <button type="button" className="btn btn-login text-light p-2">Log In</button>
+                            <button type="button" className="btn btn-nav-login text-light p-2 ml-1">Log In</button>
                         </a>
                         <a className="float-right" href="/signup">
-                            <button type="button" className="btn text-light p-2">Sign Up</button>
+                            <button type="button" className="btn btn-nav-signup text-light p-2">Sign Up</button>
                         </a>
                     </div>
                 </div>
