af239d25530eeed78aeb6914dfb3e3ede12addb3
MastProTech <uzairarif1024@gmail.com>
2021-05-22 10:18:53 +0000

    Put home on "/" path

--- src/App.js
+++ src/App.js
@@ -36,7 +36,7 @@ const App = () => {
             <Router>
                 <Navbar/>
                 <Switch>
-                    <Route path='/home' exact component={Home}/>
+                    <Route path='/' exact component={Home}/>
                     <Route path='/foryou' exact component={ForYou}/>
                     <Route path='/topstories' exact component={TopStories}/>
                     <Route path='/following' exact component={Following}/>
