1663255a2fe177f6b5acc5bf39f452b2893622fc
nicodev-git <schaefernicker@gmail.com>
2021-06-08 19:43:11 +0500

    Added Profiles

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './About.css';
 
 const About = () => {
     return (
@@ -14,9 +15,30 @@ const About = () => {
                         <img src="" alt="Hussain Shaheen" style={{width:"100%"}}></img>
                         <div className="container">
                             <h2>Hussain Shaheen</h2>
-                            <p className="title">Something comes here...</p>
-                            <p>Something comes here as well...</p>
-                            <p></p>
+                            <p className="title">Front-End Web Developer</p>
+                            <p>Areas of Expertise: ReactJS, and so on...</p>
+                            <a href="https://github.com/hussain-shaheen/" className="fa fa-github"></a>
+                        </div>
+                    </div>
+                </div>
+                <div className="column">
+                    <div className="card">
+                        <img src="" alt="Sir Muhammad Imran Saeed" style={{width:"100%"}}></img>
+                        <div className="container">
+                            <h2>Mr. Muhammad Imran Saeed</h2>
+                            <p className="title">Project Supervisor</p>
+                            <p>Assistant Professor DCS&SE, IIU</p>
+                        </div>
+                    </div>
+                </div>
+                <div className="column">
+                    <div className="card">
+                        <img src="" alt="Uzair Arif" style={{width:"100%"}}></img>
+                        <div className="container">
+                            <h2>Uzair Arif</h2>
+                            <p className="title">Front-End Web Developer</p>
+                            <p>Areas of Expertise: ReactJS, and so on...</p>
+                            <a href="https://github.com/mastprotech/" className="fa fa-github"></a>
                         </div>
                     </div>
                 </div>
