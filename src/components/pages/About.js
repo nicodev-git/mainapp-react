f68155bf5d6aba19e107deb9fe9f90f1c39525d5
nicodev-git <schaefernicker@gmail.com>
2021-06-10 14:22:13 +0500

    Changed Styling

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -12,42 +12,44 @@ const About = () => {
                 <p>Just some random text for now...</p>
             </div>
             <h2 style={{textAlign:"center"}}>Our Team</h2>
-            <div className="row">
-                <div className="column pt-3">
-                    <div className="card pt-3">
-                        <img src={Hussain} alt="Hussain Shaheen" style={{width:"100%"}} className="img-thumbnail"></img>
-                        <div className="container">
-                            <h2>Hussain Shaheen</h2>
-                            <p className="title">Front-End Web Developer</p>
-                            <p>Areas of Expertise: ReactJS, and so on...</p>
-                            <a href="https://github.com/hussain-shaheen/" className="fa fa-github"></a>
+            <div className="container-fluid p-5">
+                <div className="row">
+                    <div className="col-12 col-md-4 pt-4">
+                        <div className="card">
+                            <img src={Hussain} alt="Hussain Shaheen"className="img-thumbnail"></img>
+                            <div className="p-2">
+                                <h2>Hussain Shaheen</h2>
+                                <p className="title">Front-End Web Developer</p>
+                                <p>Areas of Expertise: ReactJS, and so on...</p>
+                                <a href="https://github.com/hussain-shaheen/"><i class="fa fa-github"></i></a>
+                            </div>
                         </div>
                     </div>
-                </div>
-                <div className="column">
-                    <div className="card">
-                        <img src={Imran} alt="Sir Muhammad Imran Saeed" style={{width:"100%"}} className="img-thumbnail"></img>
-                        <div className="container">
-                            <h2>Mr. Muhammad Imran Saeed</h2>
-                            <p className="title">Project Supervisor</p>
-                            <p>Assistant Professor DCS&SE, IIU</p>
+                    <div className="col-12 col-md-4 ">
+                        <div className="card">
+                            <img src={Imran} alt="Sir Muhammad Imran Saeed"  className="img-thumbnail"></img>
+                            <div className="p-2">
+                                <h2>Mr. Muhammad Imran Saeed</h2>
+                                <p className="title">Project Supervisor</p>
+                                <p>Assistant Professor DCS&SE, IIU</p>
+                            </div>
                         </div>
                     </div>
-                </div>
-                <div className="column pt-3">
-                    <div className="card">
-                        <img src={Uzair} alt="Uzair Arif" style={{width:"100%"}} className="img-thumbnail"></img>
-                        <div className="container">
-                            <h2>Uzair Arif</h2>
-                            <p className="title">Back-End Developer</p>
-                            <p>Areas of Expertise: FLASK, and so on...</p>
-                            <a href="https://github.com/mastprotech/" className="fa fa-github"></a>
+                    <div className="col-12 col-md-4  pt-4">
+                        <div className="card">
+                            <img src={Uzair} alt="Uzair Arif" className="img-thumbnail"></img>
+                            <div className="p-2">
+                                <h2>Uzair Arif</h2>
+                                <p className="title">Back-End Developer</p>
+                                <p>Areas of Expertise: FLASK, and so on...</p>
+                                <a href="https://github.com/mastprotech/" className="fa fa-github"></a>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
             <div className="footer">
-                <i class="fa fa-copyright" aria-hidden="true"> Newsly 2021</i>
+            &copy;Newsly 2021
             </div>
         </div>
     )
