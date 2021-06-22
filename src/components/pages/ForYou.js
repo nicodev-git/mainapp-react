8cb125e60b2e55299657530892e5c4be83a65e64
nicodev-git <schaefernicker@gmail.com>
2021-06-22 10:16:24 +0500

    Added Redirect

--- src/components/pages/ForYou.js
+++ src/components/pages/ForYou.js
@@ -1,12 +1,20 @@
 import React from 'react';
+import {Redirect} from 'react-router-dom';
 
 
-function ForYou() {
-    return (
-        <div className="foryou">
-            <h1>Personal Feeds displayed here...</h1>
-        </div>
-    )
+function ForYou({authorized}) {
+    if (!authorized) {
+        return (
+            <Redirect to='/login'/>
+        )
+    }
+    else {
+        return (
+            <div className="foryou">
+                <h1>Personal Feeds are being displayed here...</h1>
+            </div>
+        )
+    }
 }
 
 export default ForYou;
