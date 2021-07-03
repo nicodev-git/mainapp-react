026cbfe9a7f49152217e2e9cb7901ebc4aeae41b
nicodev-git <schaefernicker@gmail.com>
2021-07-03 17:53:26 +0500

    Updated Settings

--- src/components/pages/Settings.js
+++ src/components/pages/Settings.js
@@ -1,28 +1,22 @@
 import React from 'react';
 import { Redirect } from 'react-router-dom';
 
-const Settings = ({authorized}) => {
-    if (!authorized) {
-        return (
-            <Redirect to='/login'/>
-        )
-    }
-    else {
-        return (
-            <div className="container my-5">
-                <h1>
-                    <span>
-                        <button className="button btn--primary">
-                            {" "}
-                            <span>&lt;</span>
-                            Back {" "}
-                        </button> {" "}
-                        Settings
-                    </span>
-                </h1>
-            </div>
-        );
-    }
+const Settings = () => {
+    return (
+        <div className="container my-5">
+            <h2>
+                <span>
+                    <button className="btn btn-primary">
+                        {" "}
+                        <span>&lt;</span>
+                        Back {" "}
+                    </button> {" "}
+                    Settings
+                </span>
+            </h2>
+            <input className="form-control" type="text" placeholder="Search..."/>
+        </div>
+    );
 };
 
 export default Settings;
