417f2202483a52072e18a96929bf6c44cf1ff6b7
nicodev-git <schaefernicker@gmail.com>
2021-07-04 08:34:54 +0500

    Adding Options

--- src/components/pages/Settings.js
+++ src/components/pages/Settings.js
@@ -2,6 +2,45 @@ import React from 'react';
 import { Redirect } from 'react-router-dom';
 
 const Settings = () => {
+    const options = [
+        {
+            header: {
+                name : "Account",
+            },
+            values: [
+                {
+                    name: "Profile",
+                    description: "Your email address is your identity on this app and is used to log in.",
+                    tags: [
+                        "email",
+                        "username",
+                        "first name",
+                        "last name",
+                        "date of birth",
+                    ],
+                },
+            ],
+        },
+        {
+            header: {
+                name: "Applications",
+            },
+            values: [
+                {
+                    name: "Third Party Services",
+                    description: "Grant Truly Expenses access to external accounts for additional functionality.",
+                    tags: [
+
+                    ],
+                },
+            ],
+        },
+        {
+            header: {
+                
+            }
+        },
+    ]
     return (
         <div className="container my-5">
             <h2>
