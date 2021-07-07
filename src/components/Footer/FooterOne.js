7a396d348c5825df1f79393d29d2370f501405eb
nicodev-git <schaefernicker@gmail.com>
2021-07-07 22:14:25 +0500

    Created second Footer

---
+++ src/components/Footer/FooterOne.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const FooterOne = () => {
+    return (
+        <div className="footer">
+            <footer>
+                <p>Copyright &copy; Newsly 2021 | All Rights Reserved</p>
+            </footer>
+        </div>
+    );
+};
+
+export default FooterOne;
