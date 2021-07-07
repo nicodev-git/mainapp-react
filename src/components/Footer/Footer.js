718476540b8ee1ecb2b60a8c4bd9a2f7c288bcf6
nicodev-git <schaefernicker@gmail.com>
2021-07-07 15:48:03 +0500

    Created Footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,9 +1,25 @@
 import React from 'react';
+import './Footer.css';
 
 const Footer = () => {
     return (
-        <div>
-            
+        <div className="footer">
+            <footer>
+                <div className="social">
+                    <a href="#"><i className="fa fa-facebook"></i></a>
+                    <a href="#"><i className="fa fa-instagram"></i></a>
+                    <a href="#"><i className="fa fa-twitter"></i></a>
+                    <a href="#"><i className="fa fa-linkedin"></i></a>
+                </div>
+                <ul className="list-inline">
+                    <li className="list-inline-item"><a href="/">Newsly</a></li>
+                    <li className="list-inline-item"><a href="#">Privacy Policy</a></li>
+                    <li className="list-inline-item"><a href="/about">About</a></li>
+                    <li className="list-inline-item"><a href="#">Terms & Conditions</a></li>
+                    <li className="list-inline-item"><a href="/settings">Settings</a></li>
+                </ul>
+                <p className="copyright">Copyright &copy; Newsly 2021 | All Rights Reserved</p>
+            </footer>
         </div>
     );
 };
