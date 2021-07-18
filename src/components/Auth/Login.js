12357248231e150c787fa8b67ebff451c1a51f7a
MastProTech <uzairarif1024@gmail.com>
2021-07-18 11:19:24 +0000

    Partially implemented Redirect

--- src/components/Auth/Login.js
+++ src/components/Auth/Login.js
@@ -1,8 +1,8 @@
 /*  Importing React Essentials*/
-import React from 'react';
+import React, {useState} from 'react';
 
 /*  Importing useHistory hook*/
-import { useHistory } from 'react-router-dom';
+import { useHistory, Redirect} from 'react-router-dom';
 
 /*  Importing Axios*/
 import axios from 'axios';
@@ -19,61 +19,77 @@ import './Login.css';
 /*  Importing Google OAuth*/
 import GoogleAuth from './GoogleAuth';
 
+import { useCookies } from 'react-cookie';
+
 const Login = () => {
+    /*//////////////////////////////
+    NOTE: I did NOT implement jwt-login, do it yourself.
+    This file contains necessary things to use Redirect. Redirect won't work until you use it correctly.
+    1. import {Redirect} from 'react-router-dom';
+    2. Use if condition which will decide whether or not user is logged in. like Line#36
+    3. Use Redirect so that Redirect will execute instead of return ('normal HTML code') like used in this file.
+    4. Any query, ask me.
+    ////////////////////////////////*/
+    const [cookies, setCookie]=useCookies(['GoogleAuth', 'x-access-token'])
+
     let history = useHistory();
-    return (
-        <div>
-            <div className="login-box">
-                <div className="short-box">
-                    <form onSubmit = {(e) => login(e)}>
-                        <div className="login-head">
-                            <div className="login text-center">Log In to Newsly</div>
-                        </div>
-                        <div className="email">
-                            <input
-                                className="user-email"
-                                type="email"
-                                id="inputEmail"
-                                name="username"
-                                placeholder="Email"
-                                required
-                            />
-                        </div>
-                        <div className="password">
-                            <input
-                                className="user-pass"
-                                type="password"
-                                name="password"
-                                id="inputPassword"
-                                placeholder="Password"
-                                required
-                            />
-                        </div>
-                        <div className="div-btn-login">
-                            <a href={<Home/>}>
-                                <input 
-                                    className="btn btn-login bg-primary text-light" 
-                                    type="submit" 
-                                    value="Log In"
-                                    onClick={() => {history.push("/");}}
+    if(cookies["GoogleAuth"]===undefined && cookies["x-access-token"]===undefined){
+        return (
+            <div>
+                <div className="login-box">
+                    <div className="short-box">
+                        <form onSubmit = {(e) => login(e)}>
+                            <div className="login-head">
+                                <div className="login text-center">Log In to Newsly</div>
+                            </div>
+                            <div className="email">
+                                <input
+                                    className="user-email"
+                                    type="email"
+                                    id="inputEmail"
+                                    name="username"
+                                    placeholder="Email"
+                                    required
                                 />
-                            </a>
-                        </div>
-                        <div className="below-login">
-                            <div className="forgotPassword">
-                                <a href="#">Forgot Password?</a>
                             </div>
-                            <div className="btn-signup">
-                                <a href="/signup">Sign Up</a>
+                            <div className="password">
+                                <input
+                                    className="user-pass"
+                                    type="password"
+                                    name="password"
+                                    id="inputPassword"
+                                    placeholder="Password"
+                                    required
+                                />
                             </div>
-                        </div>
-                    </form>
-                    <GoogleAuth/>
+                            <div className="div-btn-login">
+                                <a href={<Home/>}>
+                                    <input 
+                                        className="btn btn-login bg-primary text-light" 
+                                        type="submit" 
+                                        value="Log In"
+                                        onClick={() => {history.push("/");}}
+                                    />
+                                </a>
+                            </div>
+                            <div className="below-login">
+                                <div className="forgotPassword">
+                                    <a href="#">Forgot Password?</a>
+                                </div>
+                                <div className="btn-signup">
+                                    <a href="/signup">Sign Up</a>
+                                </div>
+                            </div>
+                        </form>
+                        <GoogleAuth/>
+                    </div>
                 </div>
+                <FooterOne/>
             </div>
-            <FooterOne/>
-        </div>
-    );
+        );
+    } else {
+        return (<Redirect to="/"/>);
+    }
 };
 
 function login(e) {
