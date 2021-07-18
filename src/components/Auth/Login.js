d7502891f12efc4447819fbadd9a6457763df020
nicodev-git <schaefernicker@gmail.com>
2021-07-18 11:16:28 +0500

    Implemented JWT

--- src/components/Auth/Login.js
+++ src/components/Auth/Login.js
@@ -1,5 +1,8 @@
 /*  Importing React Essentials*/
-import React from 'react';
+import React, { useState } from 'react';
+
+/*  Importing useCookies hook*/
+import { useCookies } from 'react-cookie';
 
 /*  Importing useHistory hook*/
 import { useHistory } from 'react-router-dom';
@@ -21,31 +24,58 @@ import GoogleAuth from './GoogleAuth';
 
 const Login = () => {
     let history = useHistory();
+
+    const [username, setUsername]=useState('');
+    const [password, setPassword]=useState('');
+    const [cookies, setCookie]= useCookies(["user"]);
+
+    const submit = async(e) => {
+        e.preventDefault();
+        let request={
+            "username":username,
+            "password":password
+        }
+        axios.post('http://localhost:5000/login', request).then((response) => {
+            console.log(request);
+            console.log(response.data["token"]);
+            setCookie('x-access-token', response.data["token"], {path:"/"});
+            console.log("From Cookie:");
+            console.log({'x-access-token':cookies['x-access-token']});
+            
+            // alert(response.data.message);
+        }).catch((err) => {
+            console.log('Error');
+        });
+    }
+
+
     return (
         <div>
             <div className="login-box">
                 <div className="short-box">
-                    <form onSubmit = {(e) => login(e)}>
+                    <form onSubmit={submit}>
                         <div className="login-head">
                             <div className="login text-center">Log In to Newsly</div>
                         </div>
                         <div className="email">
                             <input
-                                className="user-email"
-                                type="email"
-                                id="inputEmail"
+                                className="username form-control"
+                                type="text"
+                                id="floatinginput"
                                 name="username"
-                                placeholder="Email"
+                                placeholder="Username"
+                                onChange={e => setUsername(e.target.value)}
                                 required
                             />
                         </div>
                         <div className="password">
                             <input
-                                className="user-pass"
+                                className="user-pass form-control"
                                 type="password"
                                 name="password"
                                 id="inputPassword"
                                 placeholder="Password"
+                                onChange={e => setPassword(e.target.value)}
                                 required
                             />
                         </div>
@@ -76,21 +106,21 @@ const Login = () => {
     );
 };
 
-function login(e) {
-    debugger;
-    e.preventDefault();
-    let request = {
-        email : document.getElementById("inputEmail"),
-        password : document.getElementById("inputPassword")
-    };
+// function login(e) {
+//     debugger;
+//     e.preventDefault();
+//     let request = {
+//         email : document.getElementById("inputEmail"),
+//         password : document.getElementById("inputPassword")
+//     };
 
-    axios.post("http://localhost:5000/login", request).then(
-        response => {
-            alert(response.data.message);
-        }
-    ).catch(err => {
-        console.log(err);
-    })
-};
+//     axios.post("http://localhost:5000/login", request).then(
+//         response => {
+//             alert(response.data.message);
+//         }
+//     ).catch(err => {
+//         console.log(err);
+//     })
+// };
 
 export default Login;
