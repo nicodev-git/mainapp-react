108e9a1b2a0f7f8f40fbf854154a6bece0ea0f62
nicodev-git <schaefernicker@gmail.com>
2021-07-27 21:36:54 +0500

    Temporarily removed Redirect

--- src/components/pages/ForYou.js
+++ src/components/pages/ForYou.js
@@ -2,23 +2,16 @@ import React, { useEffect } from 'react';
 import { Redirect } from 'react-router-dom';
 
 
-function ForYou({authorized}) {
+function ForYou() {
     useEffect(() => {
         document.title = "For You - Newsly"
     }, []);
 
-    if (!authorized) {
-        return (
-            <Redirect to='/login'/>
-        )
-    }
-    else {
-        return (
-            <div className="foryou container">
-                <h1>Personal Feeds are being displayed here...</h1>
-            </div>
-        )
-    }
-}
+    return (
+        <div className="foryou container">
+            <h1>Personal Feeds are being displayed here...</h1>
+        </div>
+    )
+};
 
 export default ForYou;
